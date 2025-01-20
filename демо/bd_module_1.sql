fb2cf6055130304386dc517305577feeef7f50bd
megadondon <misterman0002@gmail.com>
2025-01-20 07:41:07 +0500

    Updades files

--- демо/bd_module_1.sql
+++ демо/bd_module_1.sql
@@ -1,13 +1,29 @@
 create database gostinka;
 use gostinka;
 
--- drop database gostinka;
+create table passports(
+	IdPassport int auto_increment primary key,
+    Serial varchar(4),
+    Number varchar(6),
+    Address varchar(100),
+    WhoIssue varchar(100),
+    IssueDate date
+);
+
+create table statuses(
+	IdStatus int auto_increment primary key,
+    StatusName varchar(30)
+);
 
 create table users(
 	IdUser int auto_increment primary key,
     Firstname varchar(50),
     Lastname varchar(50),
     Patronymic varchar(50),
+    DateOfBirth date,
+    PassportId int, foreign key(PassportId) references passports(IdPassport),
+    Phone varchar(15),
+    Email varchar(256),
     Username varchar(50),
     Password varchar(50),
 	Role enum("Руководитель", "Администратор", 'Клиент', "Сотрудник", "Гость")
@@ -15,57 +31,44 @@ create table users(
 
 create table categories(
 	IdCategory int auto_increment primary key,
-    CategoryName varchar(100)
-);
-
-create table floors(
-	IdFloor int auto_increment primary key,
-    FloorName varchar(100)
+    CategoryName varchar(100),
+    PricePerDay decimal(10,2),
+    Description tinytext
 );
 
 create table rooms(
 	IdRoom int auto_increment primary key,
     RoomNumber int,
-    CategoryId int,
-    FloorId int,
-    Status enum("Занят", "Чистый", "Назначен к уборке", "Грязный"),
-    foreign key (CategoryId) references categories(IdCategory),
-    foreign key (FloorId) references floors(IdFloor)
-);
-
-create table payments(
-	IdPayment int auto_increment primary key,
-    ClientId int,
-    CategoryId int,
-    EntryDate date,
-    DepartureDate date,
-    Amount decimal(10, 2),
-    Payed decimal(10,2),
-    foreign key (ClientId) references users(IdUser),
-    foreign key (CategoryId) references categories(IdCategory)
+    CategoryId int, foreign key (CategoryId) references categories(IdCategory),
+    Floor int,
+    StatusId int, foreign key(StatusId) references statuses(IdStatus)
 );
 
 create table services(
 	IdService int auto_increment primary key,
-    ServiceName varchar (50),
+    ServiceName varchar(50),
     Description tinytext,
     Price decimal(10,2),
     InHour bool
 );
 
-create table payments_services(
-	ServiceId int,
-    PaymentId int,
-    foreign key(ServiceId) references services(IdService),
-    foreign key(PaymentId) references payments(IdPayment)
+create table bookings(
+	IdBooking int auto_increment primary key,
+    RoomId int, foreign key(RoomId) references rooms(IdRoom),
+    ClientId int, foreign key(ClientId) references users(IdUser),
+    ArrivalDate date,
+    DepartureDate date,
+    Amount decimal(10,2)
+);
+
+create table bookings_services(
+	ServiceId int, foreign key(ServiceId) references services(IdService),
+    BookingId int, foreign key(BookingId) references bookings(IdBooking)
 );
 
 create table cleaning_schedule(
 	IdCleaning int auto_increment primary key,
 	CleaningDate date,
-    FloorId int,
-    CleanerId int,
-    foreign key(FloorId ) references floors(IdFloor),
-    foreign key(CleanerId) references users(IdUser)
+    Floor int,
+    CleanerId int, foreign key(CleanerId) references users(IdUser)
 );
-
