dd74f35edf245ef35e2ab4a0c6343f1ca88478c3
RCH_02 <saifutdinow-mars@mail.ru>
2025-01-22 12:32:14 +0500

    Add pulling data from database

--- демо/Gostinka/Windows/BookingsWorkWindow.xaml.cs
+++ демо/Gostinka/Windows/BookingsWorkWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gostinka.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Gostinka.Models;
 
 namespace Gostinka.Windows
 {
@@ -19,9 +21,18 @@ namespace Gostinka.Windows
     /// </summary>
     public partial class BookingsWorkWindow : Window
     {
+        GostinkaContext context;
         public BookingsWorkWindow()
         {
             InitializeComponent();
+
+            context = new GostinkaContext();
+
+            List<Booking> bookings = context.Bookings.ToList();
+            if (bookings.Count > 0)
+            {
+                bookingsList.ItemsSource = bookings;
+            }
         }
     }
 }
