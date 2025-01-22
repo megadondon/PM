494d0b007c852fa3047400750ac1e392bcaaa71b
RCH_02 <saifutdinow-mars@mail.ru>
2025-01-22 12:38:05 +0500

    Update pulling from database code

--- демо/Gostinka/Windows/BookingsWorkWindow.xaml.cs
+++ демо/Gostinka/Windows/BookingsWorkWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Gostinka.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gostinka.Windows
 {
@@ -28,7 +29,11 @@ namespace Gostinka.Windows
 
             context = new GostinkaContext();
 
-            List<Booking> bookings = context.Bookings.ToList();
+            List<Booking> bookings = context.Bookings
+                .Include(r => r.Room)
+                .ThenInclude(c => c.Category)
+                .AsNoTracking()
+                .ToList();
             if (bookings.Count > 0)
             {
                 bookingsList.ItemsSource = bookings;
