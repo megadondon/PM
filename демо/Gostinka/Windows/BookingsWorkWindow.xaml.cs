78bc6aee57411f7142903d4ac9d858190407ad39
RCH_02 <saifutdinow-mars@mail.ru>
2025-01-24 09:05:56 +0500

    Clear code to unusual comments and imports

--- демо/Gostinka/Windows/BookingsWorkWindow.xaml.cs
+++ демо/Gostinka/Windows/BookingsWorkWindow.xaml.cs
@@ -1,22 +1,7 @@
 ﻿using Gostinka.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Gostinka.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Configuration;
-using Pomelo.EntityFrameworkCore.MySql.Query.ExpressionTranslators.Internal;
-using System.Reflection.Metadata;
 
 namespace Gostinka.Windows
 {
@@ -45,17 +30,6 @@ namespace Gostinka.Windows
                 bookingsList.ItemsSource = bookings;
             }
         }
-
-        //private void startDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    DatePicker date = sender as DatePicker;
-
-        //    if (date.SelectedDate <= DateTime.Now)
-        //    {
-        //        MessageBox.Show("Нельзя выбрать прошлую дату");
-        //        date.SelectedDate = DateTime.Now;
-        //    }
-        //}
 
         private void filterButton_Click(object sender, RoutedEventArgs e)
         {
